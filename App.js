0ae33eaca78701408cdfd5492bd81ab9a120fca0
David Leuliette <dleuliette@gmail.com>
2019-07-10 23:34:44 +0200

    🐱Add `ui-kitten`

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { mapping, light as lightTheme } from '@eva-design/eva';
+import { ApplicationProvider, Layout } from 'react-native-ui-kitten';
+import { HomeScreen } from './HomeScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+const App = () => (
+  <ApplicationProvider mapping={mapping} theme={lightTheme}>
+    <Layout style={{ flex: 1 }}>
+      <HomeScreen />
+    </Layout>
+  </ApplicationProvider>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ HomeScreen.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { Layout, Text, Button } from 'react-native-ui-kitten';
+
+export const HomeScreen = () => (
+  <Layout>
+    <Text category="h4">Welcome to UI Kitten</Text>
+    <Button>BUTTON</Button>
+  </Layout>
+);
