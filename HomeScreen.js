afef7ca6d50899a439d3b583501dab371553a232
David Leuliette <dleuliette@gmail.com>
2019-08-28 22:47:36 +0200

    Add dark mode

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { Layout, Text, Button } from 'react-native-ui-kitten';
 
-export const HomeScreen = () => (
+export const HomeScreen = props => (
   <Layout>
     <Text category="h4">Inspiration</Text>
     <Button>BUTTON</Button>
+    <Button onPress={props.toggleTheme}>Switch theme</Button>
   </Layout>
 );
